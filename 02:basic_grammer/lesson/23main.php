a4a1d9b37cefe6aeeb9f9182bb8c4a247c7d2e9a
kaifumi <kaifumi1001@gmail.com>
2020-07-12 14:29:01 +0900

    returnの使いかたメモを追加

---
+++ 02:basic_grammer/lesson/23main.php
@@ -0,0 +1,10 @@
+<?php
+
+function sum($a, $b, $c)
+{
+  // echo $a + $b + $c . PHP_EOL;
+  return $a + $b + $c;
+  echo 'Here!' . PHP_EOL;
+}
+
+echo sum(100, 300, 500) + sum(300, 400, 500) . PHP_EOL;
