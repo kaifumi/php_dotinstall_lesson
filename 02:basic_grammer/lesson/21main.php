6d8a7da6e140bd9cf3183f5267f370a36572876e
kaifumi <kaifumi1001@gmail.com>
2020-07-12 14:04:40 +0900

    関数の使い方メモを追加

---
+++ 02:basic_grammer/lesson/21main.php
@@ -0,0 +1,16 @@
+<?php
+
+function showAd()
+{
+  echo '--------' . PHP_EOL;
+  echo '---hg---' . PHP_EOL;
+  echo '--------' . PHP_EOL;
+}
+
+showAd();
+echo 'aaaaaaa' . PHP_EOL;
+echo 'iiiiiii' . PHP_EOL;
+showAd();
+echo 'uuuuuuu' . PHP_EOL;
+echo 'eeeeeee' . PHP_EOL;
+showAd();
