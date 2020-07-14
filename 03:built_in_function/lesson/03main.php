f6f4dbd0316461f5227da0dde05635c28f8e2ca3
kaifumi <kaifumi1001@gmail.com>
2020-07-14 17:13:16 +0900

    文字列を扱う関数のメモを追加

---
+++ 03:built_in_function/lesson/03main.php
@@ -0,0 +1,10 @@
+<?php
+
+$input = ' dot_install  ';
+$input = trim($input);
+
+echo strlen($input) . PHP_EOL;
+echo strpos($input, '_') . PHP_EOL;
+
+$input = str_replace('_', '--', $input);
+echo $input . PHP_EOL;
